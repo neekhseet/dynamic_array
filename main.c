ab4d880d8905f3335fcf1cdf946154e7b729866b
alex <iteeshkeen@gmail.com>
2026-03-25 17:17:44 +0500

    refactor to struct + init and free arr

--- main.c
+++ main.c
@@ -1,70 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void push_back(int **arr, size_t *capacity, int *length, int value);
-void pop_back(int *length);
-int get(int *arr, int index);
-int size(int *length);
+typedef struct {
+    size_t capacity;
+    size_t length;
+    int *arr;
 
+} VECTOR;
+
+void init_vector(VECTOR *v, size_t initial_capacity);
+void free_vector(VECTOR *v);
+void push_back(VECTOR *v, int value);
+void pop_back(VECTOR *v);
+int get(VECTOR *v, int index);
+size_t size(VECTOR *v);
+ 
 int main(void)
 {
-    size_t capacity = 10;
-    int length = 0;
-    int *arr = malloc(capacity * sizeof(int));
+    VECTOR v;
+    init_vector(&v, 10);
 
     for (int i = 0; i < 10; i++)
     {
-        push_back(&arr, &capacity, &length, i);
+        push_back(&v, i);
     }
 
     puts("After push_back");
     
     for (int i = 0; i < 10; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%d ", v.arr[i]);
     }
 
     printf("\n");
 
-    pop_back(&length);
+    pop_back(&v);
+    
     puts("After pop_back");
-
-    for (int i = 0; i < length; i++)
+    for (int i = 0; i < v.length; i++)
     {
-        printf("%d ", arr[i]);
+        printf("%d ", v.arr[i]);
     }
+
     printf("\n");
-    printf("element by 1 index %d, size of arr %d", get(arr, 1), size(length));
-    free(arr);
+
+    printf("element by 1 index %d, size of arr %d", get(&v, 1), size(&v));
+
+    free_vector(&v);
     return 0;
 }
 
-void push_back(int **arr, size_t *capacity, int *length, int value)
-{
-    if (*length == (int)*capacity)
-    {
-        *capacity = *capacity * 2;
+void init_vector(VECTOR *v, size_t initial_capacity) {
+    v->capacity = initial_capacity;
+    v->length = 0;
+    v->arr = malloc(v->capacity * sizeof(int));
 
-        int *new_arr = realloc(*arr, *capacity * sizeof(int));
+    if (v->arr == NULL) exit(1);
+}
+
+void free_vector(VECTOR *v) {
+    free(v->arr);
+
+    v->arr = NULL;
+    v->capacity = 0;
+    v->length = 0;
+}
+
+void push_back(VECTOR *v, int value)
+{
+    if (v->length == v->capacity)
+    {
+        v->capacity = v->capacity * 2;
+
+        int *new_arr = realloc(v->arr, v->capacity * sizeof(int));
         if (new_arr == NULL) return;
 
-        *arr = new_arr;
+        v->arr = new_arr;
     }
 
-    (*arr)[*length] = value;
-    (*length)++;
+    (v->arr)[v->length] = value;
+    (v->length)++;
 }
 
-void pop_back(int *length)
+void pop_back(VECTOR *v)
 {
-    if (*length != 0)
-        *length = *length - 1;
+    if (v->length != 0)
+        v->length -= 1;
 }
 
-int get(int *arr, int index) {
-    return arr[index];
+int get(VECTOR *v, int index) {
+    return v->arr[index];
 }
 
-int size(int *length) {
-    return *length;
+size_t size(VECTOR *v) {
+    return v->length;
 }
